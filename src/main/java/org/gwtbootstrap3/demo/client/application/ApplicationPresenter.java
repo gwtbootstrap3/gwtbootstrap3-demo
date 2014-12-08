60a0eaebd3eb2b6d609c7179ccda3df78d6a97c3
Joshua Godi <Joshua.Godi@nisc.coop>
2014-12-08 14:25:56 -0600

    Fixes #37 - Scrolling when navigating through the demo

--- src/main/java/org/gwtbootstrap3/demo/client/application/ApplicationPresenter.java
+++ src/main/java/org/gwtbootstrap3/demo/client/application/ApplicationPresenter.java
@@ -20,7 +20,13 @@ package org.gwtbootstrap3.demo.client.application;
  * #L%
  */
 
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.Window;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
@@ -52,6 +58,19 @@ public class ApplicationPresenter extends Presenter<ApplicationPresenter.MyView,
     ApplicationPresenter(final EventBus eventBus,
                          final MyView view,
                          final MyProxy proxy) {
-        super(eventBus, view, proxy, RevealType.RootLayout);
+        super(eventBus, view, proxy, RevealType.Root);
+
+        // Making the window scroll to top on every page change
+        History.addValueChangeHandler(new ValueChangeHandler<String>() {
+            @Override
+            public void onValueChange(ValueChangeEvent<String> event) {
+                Scheduler.get().scheduleDeferred(new Command() {
+                    @Override
+                    public void execute() {
+                        Window.scrollTo(0, 0);
+                    }
+                });
+            }
+        });
     }
 }
