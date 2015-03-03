3220ff87302aaf2982ca8be9a9b3546dbca160f8
Xiaodong Sun <xsun@wallix.com>
2015-03-03 17:42:20 +0100

    Added tooltip, precision, and all event handlers demo

--- src/main/java/org/gwtbootstrap3/demo/client/application/extras/SliderView.java
+++ src/main/java/org/gwtbootstrap3/demo/client/application/extras/SliderView.java
@@ -20,16 +20,24 @@ package org.gwtbootstrap3.demo.client.application.extras;
  * #L%
  */
 
+import org.gwtbootstrap3.client.ui.html.Paragraph;
 import org.gwtbootstrap3.extras.slider.client.ui.Range;
 import org.gwtbootstrap3.extras.slider.client.ui.RangeSlider;
 import org.gwtbootstrap3.extras.slider.client.ui.Slider;
 import org.gwtbootstrap3.extras.slider.client.ui.base.FormatterCallback;
+import org.gwtbootstrap3.extras.slider.client.ui.base.event.SlideDisabledEvent;
+import org.gwtbootstrap3.extras.slider.client.ui.base.event.SlideEnabledEvent;
+import org.gwtbootstrap3.extras.slider.client.ui.base.event.SlideEvent;
+import org.gwtbootstrap3.extras.slider.client.ui.base.event.SlideStartEvent;
+import org.gwtbootstrap3.extras.slider.client.ui.base.event.SlideStopEvent;
 
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewImpl;
@@ -42,6 +50,9 @@ public class SliderView extends ViewImpl implements SliderPresenter.MyView {
     @UiField Slider basicExample;
     @UiField RangeSlider rangeExample;
     @UiField Slider formatterExample;
+    @UiField Slider eventExample;
+    @UiField RangeSlider eventRangeExample;
+    @UiField FlowPanel logRow;
 
     interface Binder extends UiBinder<Widget, SliderView> {}
 
@@ -74,7 +85,7 @@ public class SliderView extends ViewImpl implements SliderPresenter.MyView {
 
     @UiHandler("isEnabled")
     void onIsEnabled(ClickEvent event) {
-    	Window.alert("" + basicExample.isEnabled());
+        Window.alert("" + basicExample.isEnabled());
     }
 
     @UiHandler("getValue")
@@ -115,5 +126,96 @@ public class SliderView extends ViewImpl implements SliderPresenter.MyView {
     @UiHandler("rangeSetValue")
     void onRangeSetValue(ClickEvent event) {
         rangeExample.setValue(new Range(400, 700));
+    }
+
+    @UiHandler("eventExample")
+    void onSlide(SlideEvent<Double> event) {
+        addEventLog("slide", "value = " + event.getValue());
+    }
+
+    @UiHandler("eventExample")
+    void onSlideStart(SlideStartEvent<Double> event) {
+        addEventLog("slideStart", "value = " + event.getValue());
+    }
+
+    @UiHandler("eventExample")
+    void onSlideStop(SlideStopEvent<Double> event) {
+        addEventLog("slideStop", "value = " + event.getValue());
+    }
+
+    @UiHandler("eventExample")
+    void onChange(ValueChangeEvent<Double> event) {
+        addEventLog("change", "value = " + event.getValue());
+    }
+
+    @UiHandler("eventExample")
+    void onSlideEnabled(SlideEnabledEvent event) {
+        addEventLog("slideEnabled", "");
+    }
+
+    @UiHandler("eventExample")
+    void onSlideDisabled(SlideDisabledEvent event) {
+        addEventLog("slideDisabled", "");
+    }
+
+    @UiHandler("eventToggle")
+    void onEventToggle(ClickEvent event) {
+        eventExample.toggle();
+    }
+
+    @UiHandler("eventSetValue")
+    void onEventSetValue(ClickEvent event) {
+        eventExample.setValue(50D);
+    }
+
+    @UiHandler("eventRangeExample")
+    void onRangeSlide(SlideEvent<Range> event) {
+        addEventLog("slide", "value = " + event.getValue());
+    }
+
+    @UiHandler("eventRangeExample")
+    void onRangeSlideStart(SlideStartEvent<Range> event) {
+        addEventLog("slideStart", "value = " + event.getValue());
+    }
+
+    @UiHandler("eventRangeExample")
+    void onRangeSlideStop(SlideStopEvent<Range> event) {
+        addEventLog("slideStop", "value = " + event.getValue());
+    }
+
+    @UiHandler("eventRangeExample")
+    void onRangeChange(ValueChangeEvent<Range> event) {
+        addEventLog("change", "value = " + event.getValue());
+    }
+
+    @UiHandler("eventRangeExample")
+    void onRangeSlideEnabled(SlideEnabledEvent event) {
+        addEventLog("slideEnabled", "");
+    }
+
+    @UiHandler("eventRangeExample")
+    void onRangeSlideDisabled(SlideDisabledEvent event) {
+        addEventLog("slideDisabled", "");
+    }
+
+    @UiHandler("eventRangeToggle")
+    void onEventRangeToggle(ClickEvent event) {
+        eventRangeExample.toggle();
+    }
+
+    @UiHandler("eventRangeSetValue")
+    void onEventRangeSetValue(ClickEvent event) {
+        eventRangeExample.setValue(new Range(20, 50));
+    }
+
+    private void addEventLog(String eventName, String logSuffix) {
+        final Paragraph logEntry = new Paragraph();
+        logEntry.setHTML("<b>" + eventName + "</b> event fired! " + logSuffix);
+        logRow.add(logEntry);
+    }
+
+    @UiHandler("clearLogButton")
+    void onClickclearLogButton(ClickEvent event) {
+        logRow.clear();
     }
 }
