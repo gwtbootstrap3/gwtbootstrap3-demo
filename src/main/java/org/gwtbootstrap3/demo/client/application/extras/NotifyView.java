d08661833148dfbba5a2818753bd7664183a4ada
Xiaodong Sun <xiaodong.sun@wallix.com>
2015-03-15 17:16:06 +0100

    First version of bootstrap notify demo

--- src/main/java/org/gwtbootstrap3/demo/client/application/extras/NotifyView.java
+++ src/main/java/org/gwtbootstrap3/demo/client/application/extras/NotifyView.java
@@ -9,9 +9,9 @@ package org.gwtbootstrap3.demo.client.application.extras;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -61,12 +61,12 @@ public class NotifyView extends ViewImpl implements NotifyPresenter.MyView {
 
     @UiHandler("basicNotifyWithTitleIcon")
     public void handleBasicNotifyWithTitleIcon(final ClickEvent event) {
-    	Notify.notify("Title", "Message", IconType.SMILE_O);
+        Notify.notify("Title", "Message", IconType.SMILE_O);
     }
 
     @UiHandler("changeBackgroundAndDismiss")
     public void handleChangeBackgroundAndDismiss(final ClickEvent event) {
-        NotifySettings settings = new NotifySettings();
+        NotifySettings settings = NotifySettings.newSettings();
         settings.setType(NotifyType.SUCCESS);
         settings.setAllowDismiss(false);
         Notify.notify("Title", "Message", IconType.SMILE_O, settings);
@@ -75,8 +75,8 @@ public class NotifyView extends ViewImpl implements NotifyPresenter.MyView {
     @UiHandler("positionAndLink")
     public void handlePositionAndFormatting(final ClickEvent event) {
 
-    	NotifySettings settings = new NotifySettings();
-    	settings.setPlacement(NotifyPlacement.TOP_CENTER);
+        NotifySettings settings = NotifySettings.newSettings();
+        settings.setPlacement(NotifyPlacement.TOP_CENTER);
 
         Notify.notify("Title", "Message", IconType.SMILE_O, "https://github.com/gwtbootstrap3/gwtbootstrap3", settings);
 
@@ -85,9 +85,9 @@ public class NotifyView extends ViewImpl implements NotifyPresenter.MyView {
     @UiHandler("animationAndOffset")
     public void handleAnimationAndOffset(final ClickEvent event) {
 
-    	NotifySettings settings = new NotifySettings();
-    	settings.setAnimation(Animation.TADA, Animation.LIGHTSPEED_OUT);
-    	settings.setOffset(200, 140);
+        NotifySettings settings = NotifySettings.newSettings();
+        settings.setAnimation(Animation.TADA, Animation.LIGHTSPEED_OUT);
+        settings.setOffset(200, 140);
 
         Notify.notify("Title", "Message", IconType.SMILE_O, settings);
 
@@ -96,8 +96,8 @@ public class NotifyView extends ViewImpl implements NotifyPresenter.MyView {
     @UiHandler("template")
     public void handleTemplate(final ClickEvent event) {
 
-    	NotifySettings settings = new NotifySettings();
-    	settings.setTemplate("<div data-notify=\"container\" class=\"alert\" role=\"alert\">\n" +
+        NotifySettings settings = NotifySettings.newSettings();
+        settings.setTemplate("<div data-notify=\"container\" class=\"alert\" role=\"alert\">\n" +
                 "\t\t<button type=\"button\" class=\"close\" data-notify=\"dismiss\">\n" +
                 "\t\t\t<span aria-hidden=\"true\">×</span>\n" +
                 "\t\t\t<span class=\"sr-only\">Close</span>\n" +
@@ -108,7 +108,7 @@ public class NotifyView extends ViewImpl implements NotifyPresenter.MyView {
                 "\t\t<a href=\"#\" data-notify=\"url\"></a>\n" +
                 "\t</div>");
 
-    	settings.makeDefault();
+        settings.makeDefault();
 
         Notify.notify("Title", "Message", IconType.SMILE_O);
 
