c838772f683862dd5c2ffdbe94b24b5935906d1c
Xiaodong Sun <xiaodong.sun@wallix.com>
2016-01-09 16:57:51 +0100

    Updated bootbox demo with new APIs

--- src/main/java/org/gwtbootstrap3/demo/client/application/extras/BootboxJSView.java
+++ src/main/java/org/gwtbootstrap3/demo/client/application/extras/BootboxJSView.java
@@ -4,7 +4,7 @@ package org.gwtbootstrap3.demo.client.application.extras;
  * #%L
  * GwtBootstrap3
  * %%
- * Copyright (C) 2013 GwtBootstrap3
+ * Copyright (C) 2016 GwtBootstrap3
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,20 @@ package org.gwtbootstrap3.demo.client.application.extras;
  * #L%
  */
 
+import org.gwtbootstrap3.client.ui.ListBox;
+import org.gwtbootstrap3.client.ui.constants.ButtonType;
+import org.gwtbootstrap3.extras.bootbox.client.Bootbox;
+import org.gwtbootstrap3.extras.bootbox.client.callback.ConfirmCallback;
+import org.gwtbootstrap3.extras.bootbox.client.callback.PromptCallback;
+import org.gwtbootstrap3.extras.bootbox.client.callback.SimpleCallback;
+import org.gwtbootstrap3.extras.bootbox.client.options.AlertOptions;
+import org.gwtbootstrap3.extras.bootbox.client.options.BootboxLocale;
+import org.gwtbootstrap3.extras.bootbox.client.options.BootboxSize;
+import org.gwtbootstrap3.extras.bootbox.client.options.ConfirmOptions;
+import org.gwtbootstrap3.extras.bootbox.client.options.DialogOptions;
+import org.gwtbootstrap3.extras.bootbox.client.options.PromptOptions;
+
+import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -28,43 +42,48 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewImpl;
-import org.gwtbootstrap3.client.ui.Button;
-import org.gwtbootstrap3.extras.bootbox.client.Bootbox;
-import org.gwtbootstrap3.extras.bootbox.client.callback.AlertCallback;
-import org.gwtbootstrap3.extras.bootbox.client.callback.ConfirmCallback;
-import org.gwtbootstrap3.extras.bootbox.client.callback.PromptCallback;
 
 /**
  * @author lyhcode
  */
 public class BootboxJSView extends ViewImpl implements BootboxJSPresenter.MyView {
 
-    @UiField
-    Button alertButton;
-    @UiField
-    Button alertCallbackButton;
-    @UiField
-    Button confirmButton;
-    @UiField
-    Button promptButton;
+    @UiField ListBox localeBox;
+
+    @UiHandler("localeBox")
+    void onChangeLanguage(final ChangeEvent event) {
+        String locale = localeBox.getSelectedValue();
+        Bootbox.setLocale(BootboxLocale.valueOf(locale));
+    }
 
     @UiHandler("alertButton")
-    public void handleAlertButton(final ClickEvent event) {
+    void handleAlertButton(final ClickEvent event) {
         Bootbox.alert("Hello World");
     }
 
     @UiHandler("alertCallbackButton")
-    public void handleAlertCallbackButton(final ClickEvent event) {
-        Bootbox.alert("Hello World", new AlertCallback() {
+    void handleAlertCallbackButton(final ClickEvent event) {
+        Bootbox.alert("Alert With Callback", new SimpleCallback() {
             @Override
             public void callback() {
-                Window.alert("Hello World Callback");
+                Window.alert("My Alert Callback");
             }
         });
     }
 
+    @UiHandler("alertOptionsButton")
+    void handleAlertOptionsButton(final ClickEvent event) {
+        AlertOptions options = AlertOptions.newOptions("Alert With Options");
+        options.setSize(BootboxSize.LARGE);
+        options.setAnimate(false);
+        options.setBackdrop(false);
+        options.setTitle("Custome Alert Title");
+        options.setCloseButton(false);
+        Bootbox.alert(options);
+    }
+
     @UiHandler("confirmButton")
-    public void handleConfirmButton(final ClickEvent event) {
+    void handleConfirmButton(final ClickEvent event) {
         Bootbox.confirm("Hello World", new ConfirmCallback() {
             @Override
             public void callback(final boolean result) {
@@ -73,8 +92,25 @@ public class BootboxJSView extends ViewImpl implements BootboxJSPresenter.MyView
         });
     }
 
+    @UiHandler("confirmOptionsButton")
+    void handleConfirmOptionsButton(final ClickEvent event) {
+        ConfirmOptions options = ConfirmOptions.newOptions("Confirm With Options");
+        options.setSize(BootboxSize.SMALL);
+        options.setAnimate(false);
+        options.setBackdrop(false);
+        options.setTitle("Custome Confirm Title");
+        options.setCloseButton(false);
+        options.setCallback(new ConfirmCallback() {
+            @Override
+            public void callback(final boolean result) {
+                Window.alert("Return: " + result);
+            }
+        });
+        Bootbox.confirm(options);
+    }
+
     @UiHandler("promptButton")
-    public void handlePromptButton(final ClickEvent event) {
+    void handlePromptButton(final ClickEvent event) {
         Bootbox.prompt("Hello World", new PromptCallback() {
             @Override
             public void callback(final String result) {
@@ -83,19 +119,46 @@ public class BootboxJSView extends ViewImpl implements BootboxJSPresenter.MyView
         });
     }
 
+    @UiHandler("promptOptionsButton")
+    void handlePromptOptionsButton(final ClickEvent event) {
+        PromptOptions options = PromptOptions.newOptions("Prompt With Options");
+        options.setAnimate(false);
+        options.setBackdrop(false);
+        options.setTitle("Custome Prompt Title");
+        options.setCloseButton(false);
+        options.setCallback(new PromptCallback() {
+            @Override
+            public void callback(String result) {
+                Window.alert("Return: " + result);
+            }
+        });
+        Bootbox.prompt(options);
+    }
+
     @UiHandler("dialogButton")
-    public void handleButton(ClickEvent event) {
-        Bootbox.Dialog.create()
-            .setMessage("Dialog message!")
-            .setCloseButton(false)
-            .setTitle("<i class=\"fa fa-info-circle fa-3x text-info\" style=\"vertical-align: middle; padding-right:10px;\"></i>Dialog title")
-            .addButton("OK", "btn-primary", new AlertCallback() {
-                @Override
-                public void callback() {
-                    Window.alert("Ok button selected");
-                }})
-            .addButton("Cancel", "btn-danger")
-            .show();
+    void handleDialogButton(ClickEvent event) {
+        DialogOptions options = DialogOptions.newOptions("I am a custom dialog");
+        options.setTitle("Custom title");
+        options.setOnEscape(new SimpleCallback() {
+            @Override
+            public void callback() {
+                Window.alert("On Escape!");
+            }
+        });
+        options.addButton("Success!", ButtonType.SUCCESS.getCssName(), new SimpleCallback() {
+            @Override
+            public void callback() {
+                Window.alert("Success callback!");
+            }
+        });
+        options.addButton("Danger!", ButtonType.DANGER.getCssName(), new SimpleCallback() {
+            @Override
+            public void callback() {
+                Window.alert("Danger callback!");
+            }
+        });
+        options.addButton("Click ME!");
+        Bootbox.dialog(options);
     }
 
     interface Binder extends UiBinder<Widget, BootboxJSView> {
@@ -103,6 +166,13 @@ public class BootboxJSView extends ViewImpl implements BootboxJSPresenter.MyView
 
     @Inject
     BootboxJSView(final Binder uiBinder) {
+
         initWidget(uiBinder.createAndBindUi(this));
+
+        // Populate locales
+        for (BootboxLocale locale : BootboxLocale.values()) {
+            localeBox.addItem(locale.getLocale(), locale.name());
+        }
+        localeBox.setSelectedIndex(BootboxLocale.getDefault().ordinal());
     }
 }
