74b7982e537c3cde7bea556c757b6c4b2debfa52
Steven Jardine <steve@mjnservices.com>
2016-02-24 14:42:13 -0700

    Work on automatic generation of demo site and javadoc.

--- .utility/update_demo_site.sh
+++ .utility/update_demo_site.sh
@@ -2,20 +2,28 @@
 set -ev
 if [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
-if [[ -z "$GH_TOKEN" ]]; then
+if [ -z "$GH_TOKEN" ]; then
 echo -e "GH_TOKEN is not set"
 exit 1
 fi	
-
-if [ ! -f $TRAVIS_BUILD_DIR/target/gwtbootstrap3-demo-*.war ]; then
-echo -e "demo war file not found."
-exit 1
-fi	
-												
+			
 echo -e "Publishing demo to gh-pages . . .\n"
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
+
+# clone and build the demo application.
+cd $HOME
+rm -rf demo
+git clone --quiet --branch=master https://$GH_TOKEN@github.com/gwtbootstrap3/gwtbootstrap3-demo demo > /dev/null
+cd demo
+mvn clean package || { echo -e "gwtbootstrap3-demo build failed" ; exit 1; }
+
+# check to make sure the demo war file is present.
+if [ ! -f $HOME/demo/target/gwtbootstrap3-demo-*.war ]; then
+echo -e "gwtbootstrap3-demo war file not found."
+exit 1
+fi	
 
 # clone the gh-pages branch.
 cd $HOME
@@ -41,7 +49,7 @@ git rm -rf ./snapshot/WEB-INF
 fi
 
 # copy the new GwtBootstrap3Demo the snapshot dir.
-unzip -u $TRAVIS_BUILD_DIR/target/gwtbootstrap3-demo-*.war -d ./snapshot/
+unzip -u $HOME/demo/target/gwtbootstrap3-demo-*.war -d ./snapshot/
 mv -f  ./snapshot/GwtBootstrap3Demo.html ./snapshot/index.html
 rm -rf ./snapshot/META-INF 
 rm -rf ./snapshot/WEB-INF
